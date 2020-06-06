a1eb402acb2036a1909a3c4d23cac93c2edff8a8
casey0808 <casey_c0808@outlook.com>
2020-06-06 12:43:45 +0800

    Updates

---
+++ precache-manifest.8907d5310f24ee32b76252ae0fdc6905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c4fa3c347fda0a874d7c4ffad1c8ab",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "1e4108a41f23187855f6",
+    "url": "/pomodoro-clock/static/css/main.a24877a3.chunk.css"
+  },
+  {
+    "revision": "887a5e4e60fedb995340",
+    "url": "/pomodoro-clock/static/js/2.e1f290b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.e1f290b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4108a41f23187855f6",
+    "url": "/pomodoro-clock/static/js/main.990e177b.chunk.js"
+  },
+  {
+    "revision": "743f6ad71378fe6a80d1",
+    "url": "/pomodoro-clock/static/js/runtime-main.fda66761.js"
+  }
+]);
